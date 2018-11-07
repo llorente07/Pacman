a21da2c69d44306ed84421f8d9ec4a23b03edf02
Lorente35 <Lorente35@LAPTOP-30B2IGTD>
2018-11-07 20:00:35 +0100

    Tasca 2 FALTA QUE DIBUJE

--- public_html/codi.js
+++ public_html/codi.js
@@ -1,16 +1,15 @@
 /* DIRECCIO DELS FANTASMAS I JUGADOR.
-// 1 a dalt     ^
-// 2 dreta      >
-// 3 a baix     v
-// 4 esquerra   <
+ // 1 Dalt       ^
+ // 2 Dreta      >
+ // 3 Baix       v 
+ // 4 Esquerra   <
  */
 
 //X , Y jugador, direccio Inici i la que vol Anar
-var lorente  = new Array();
+var jugador = new Array();
 var fantasma1 = new Array();
 var fantasma2 = new Array();
 var fantasma3 = new Array();
-
 // Creacio del Mapa 
 var mapa = new Array();
 mapa = [
@@ -47,26 +46,101 @@ mapa = [
     mapa [1] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
+function Pacman(){
+    IniciJugador(jugador);
+    IniciFantasma(fantasma1);
+    IniciFantasma(fantasma2);
+    IniciFantasma(fantasma3);
+    mapa1();
+}
 
-
-function mapa1(){
-    for(var i = 0; i < 29; i++){
-        for(var j = 0; j < 29; j++){
-            if(mapa[i][j] == 1){
-                document.write("*");
+// FUNCION PER DIBUIXAR EL MAPA.
+function mapa1() {
+    for (var i = 0; i < 30; i++) {
+        for (var j = 0; j < 30; j++) {
+            if (mapa[i][j] == 1) {
+                document.write("&nbsp;");
             }
-            else document.write("&nbsp;");                       
+            else if(mapa[i][j] == 2) {
+                document.write("P");
+            }
+            else if(mapa[i][j] == 3) {
+                document.write("F");
+            }
+            else document.write("*");
         }
         document.write("<br>");
     }
 }
 
-function IniciJugador(){
-    
+function ComPosicio(y, x) {
+    var comprobar = false;
+    if (mapa[x][y] == 1) {
+        comprobar = true;
+    }
+    return comprobar;
 }
 
-function IniciFantasma(){
+function IniciJugador(lorente) {
+    var x, y;
+
+    do {
+        x = Math.floor((Math.random() * 30) + 0);
+        y = Math.floor((Math.random() * 30) + 0);
+    } while (!ComPosicio(y, x))
+
+    //VALOR A LA POSICIO DEL JUGADOR.
+    lorente [1] = x;
+    lorente [2] = y;
+    lorente [3] = DirIniciAlea(y, x);
+    lorente [4] = DirIniciAlea(y, x);
     
+    //DINTRE DEL MAPA
+    mapa[y][x] = 2;
 }
 
+function IniciFantasma(fantasma) {
+    var x, y;
+    var Espai = false;
 
+    do {
+        x = Math.floor((Math.random() * 30) + 0);
+        y = Math.floor((Math.random() * 30) + 0);
+    } while (!ComPosicio(y, x))
+
+    //VALORS FANTASMA
+    fantasma[1] = x;
+    fantasma[2] = y;
+    fantasma[3] = DirIniciAlea(y, x);
+
+    //FIQUEN AL MAPA
+    mapa[y][x] = 3;
+}
+
+function DirIniciAlea(y, x) {
+    var dir = new Array();
+    var correcta = false;
+    var DirInici;
+    var aux;
+    
+    //Comprobacio de Direccions.
+    if(ComPosicio(y + 1 x) = 1) dir[1] = 1;
+    else dir[1] = 0;
+    if(ComPosicio(y, x + 1) = 1) dir[2] = 1;
+    else dir[2] = 0;
+    if(ComPosicio(y - 1, x) = 1) dir[3] = 1;
+    else dir[3] = 0;
+    if(ComPosicio(y, x - 1) = 1) dir[4] = 1;
+    else dir[4] = 0;
+
+    do {
+        aux = Math.floor((Math.random() * 4) + 1);
+        if(dir [aux] = 1) {
+            correcta = true;
+            DirInici = aux;
+        }
+    } while (!correcta)
+
+    //retornem la direccio inicial
+    return DirInici;
+}
